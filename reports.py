973a572b4a955a6882e6be41251c119c7916bfb1
shuribuzz <shurabalaganov1307@gmail.com>
2018-09-13 20:35:39 +0500

    .

--- reports.py
+++ reports.py
@@ -31,18 +31,19 @@ def visitorsfresh():
     result2 = cursor.fetchall()
     print('Посетители из этой страны чаще всего интересуются товарами из категории “fresh_fish: '+ str(freq(result2)))
 
-
+'''
 # 3.В какое время суток чаще всего просматривают категорию “frozen_fish”?
 def timesofday():
     cursor.execute("SELECT Time FROM testtable WHERE Category = 'frozen_fish'")
     result3 = cursor.fetchall()
     print(freq(result3))
-
+'''
 
 # 4.Какое максимальное число запросов на сайт за астрономический час
 #  (c 00 минут 00 секунд до 59 минут 59 секунд)?
 
 # 5.Товары из какой категории чаще всего покупают совместно с товаром из категории “semi_manufactures”?
+
 '''
 # 6.Сколько брошенных (не оплаченных) корзин имеется?
 cursor.execute("SELECT IP FROM testtable WHERE Category LIKE 'cart?goods%'")
@@ -64,50 +65,4 @@ def doublesuccesspay():
                 arr.append(i)
     print('Количество пользователей, которые совершали повторные покупки: ' + str(len(arr)))
 
-'''
-##################### 6
-cartIP_arr = []
-cartgoods_arr = []
-
-cursor.execute("SELECT IP FROM testtable WHERE Category LIKE 'cart?goods%'")
-cartIP_tulpe = cursor.fetchall()
-
-for row in cartIP_tulpe:
-    for x in row:
-        cartIP_arr.append(str(x))
-
-for i in cartIP_arr:
-    cursor.execute("SELECT Category FROM testtable WHERE IP=:IP", {"IP": i})
-    cartgoods = cursor.fetchall()
-    cartgoods_arr.append(cartgoods)
-
-counts = [sum(1 for a, in array if a.startswith("cart?goods")) for array in cartgoods_arr]
-
-print(len(cartIP_arr))
-print(Counter(cartIP_arr))
-print(len(counts))
-
-
-payIP_arr = []
-paygoods_arr = []
-
-cursor.execute("SELECT IP FROM testtable WHERE Category LIKE 'success_pay%'")
-payIP_tulpe = cursor.fetchall()
-
-for row in payIP_tulpe:
-    for x in row:
-        payIP_arr.append(str(x))
-
-for i in payIP_arr:
-    cursor.execute("SELECT Category FROM testtable WHERE IP=:IP", {"IP": i})
-    paygoods = cursor.fetchall()
-    paygoods_arr.append(paygoods)
-
-counts = [sum(1 for a, in array if a.startswith("success_pay")) for array in paygoods_arr]
-
-print(len(payIP_arr))
-print(Counter(payIP_arr))
-print(len(counts))
-'''
-################################
 #tesdb.close()
